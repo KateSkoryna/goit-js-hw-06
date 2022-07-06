d46153a2600564f1eb0c323938fe6f9a13a561d7
Kateryna Skoryna <k.skoryna@gmail.com>
2022-07-06 14:41:37 +0200

    added changes to 10 task

--- js/task-10.js
+++ js/task-10.js
@@ -1,18 +1,17 @@
 const createBtn = document.querySelector('[data-create]');
 const destroyBtn = document.querySelector('[data-destroy]');
-const inputNumber = document.querySelector('input');
+const inputEl = document.querySelector('input');
 const listEl = document.querySelector('#controls')
 const containerEl = document.querySelector('#boxes')
 
 
-createBtn.addEventListener('click', createBoxes);
+createBtn.addEventListener('click', () => createBoxes(inputEl.value));
 destroyBtn.addEventListener('click', destroyBoxes);
 
-function createBoxes() {
-  const amount = inputNumber.value;
-  let size = 20;
-  
-  for (let i = 1; i <= amount; i += 1) {
+function createBoxes(amount) {
+  let size = 30;
+
+  for (let i = 0; i < amount; i += 1) {
     size = size + 10;
     const box = document.createElement('div');
     box.style.backgroundColor = `${getRandomHexColor()}`
@@ -26,7 +25,7 @@ function createBoxes() {
 
 function destroyBoxes() {
   containerEl.innerHTML = "";
-  inputNumber.value = "";
+  inputEl.value = "";
 }
 
 function getRandomHexColor() {
@@ -34,7 +33,3 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
-
-
-
-
